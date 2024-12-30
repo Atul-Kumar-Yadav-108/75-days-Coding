7c516dd76b45ae6658d7aed2735c9cb0b9391da1
Atul Kumar Yadav <atulanirudhyadav@gmail.com>
2024-12-30 23:24:45 +0530

    4/75 days Produc of array except itself , changes question added

--- D4_product_of_arr_except_itself.js
+++ D4_product_of_arr_except_itself.js
@@ -1,3 +1,18 @@
+/**
+ *              Product of Array Except Self
+ *  Given an integer array nums, return an array answer such that
+ *  answer[i] is equal to the product of all the elements of nums
+ *  except nums[I].
+ *  The product of any prefix or suffix of nums is guaranteed to fit in a
+ *  32-bit integer.
+ * You must write an algorithm that runs in O(n) time and without
+ * using the division operation.
+ * 
+ * Input: nums = [1,2,3,4]
+ * Output: [24,12,8,6]
+ * 
+ */
+
 function productOfArr(arr){
     let resArr = [];
     for(let i=0; i < arr.length; i++){
